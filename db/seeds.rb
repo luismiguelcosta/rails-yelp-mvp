b5b4c07fccb7c248aa58d5f5e10409942cc8ebc4
luis chagas <luischagas78@gmail.com>
2018-10-24 12:28:33 -0400

    Yelp MVP challenge incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'da mimmo', address: 'bergamo alta', phone_number: '111213141516', category: 'italian')
+Restaurant.create(name: 'bai wei guan', address: 'via quarenghi bergamo', phone_number: '212223242526', category: 'chinese')
+Restaurant.create(name: 'kura', address: 'via paglia giorgio e guido bergamo', phone_number: '313233343536', category: 'japanese')
+Restaurant.create(name: 'verdier jean dominique', address: 'via don luigi palazzolo bergamo', phone_number: '414243444546', category: 'french')
+Restaurant.create(name: 'b-brasserie', address: 'bariano bergamo', phone_number: '515253545556', category: 'belgian')
